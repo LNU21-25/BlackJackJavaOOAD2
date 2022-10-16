7e0f8991fc8b8555c094936475aca7026665a397
hs223nr <hs223nr@student.lnu.se>
2022-10-16 11:50:09 +0200

    transfer from other project

---
+++ app/src/main/java/controller/App.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.Game;
+import view.EnglishView;
+import view.SwedishView;
+import view.View;
+
+/**
+ * Starts the application using the console.
+ */
+public class App {
+  /**
+   * Starts the game.
+
+  * @param args Not used.
+  */
+  public static void main(String[] args) {
+
+    Game g = new Game(); 
+    View v = new EnglishView();
+    //View swe = new SwedishView();
+    Player ctrl = new Player();
+
+    while (ctrl.play(g, v)) {
+
+    }
+  }
+}
